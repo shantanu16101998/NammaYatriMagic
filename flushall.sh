bc268bd93bb6348e3434d50bc443a82b28b07fa3
Shantanu Mukherjee <shantanu.mukherjee@juspay.in>
2023-06-23 18:26:25 +0530

    commit

--- flushall.sh
+++ flushall.sh
@@ -6,11 +6,3 @@ echo "[+] Flushed Redis Cluster 2"
 redis-cli -p 30002 FLUSHALL
 echo "[+] Flushed Redis Cluster 3"
 redis-cli -p 30003 FLUSHALL
-# echo "[+] Flushed Redis Cluster 4"
-# redis-cli -p 30004 FLUSHALL
-# echo "[+] Flushed Redis Cluster 5"
-# redis-cli -p 30005 FLUSHALL
-# echo "[+] Flushed Redis Cluster 6"
-# redis-cli -p 30006 FLUSHALL
-# echo "[+] Flushed Redis Cluster 7"
-# redis-cli -p 30007 FLUSHALL
