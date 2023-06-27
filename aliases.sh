f4958799785868068a373a2335a8b5e3ecefeb0c
Shantanu Mukherjee <shantanu.mukherjee@juspay.in>
2023-06-27 14:24:46 +0530

    commit

--- aliases.sh
+++ aliases.sh
@@ -1,24 +1,18 @@
 # copy paste in your zshrc file 
-# change juspay to your username
 
 #!/bin/bash
 
 # Specify the aliases
+
+magic_root=""
+project_root=""
+
 aliases=(
     "cba='cabal build all'"
     "cc='cabal clean'"
     "cdh='cd ~'"
-    "code-backend='code /Users/juspay/nammayatri/Backend'"
-    "code-config='code /Users/juspay/nix-config'"
-    "code-custom='code ~/.oh-my-zsh/custom/'"
-    "code-driver='code /Users/juspay/nammayatri/Backend/app/provider-platform/dynamic-offer-driver-app/'"
-    "code-kernel='code /Users/juspay/shared-kernel'"
-    "code-ny='code /Users/juspay/nammayatri/'"
-    "code-other-driver='code /Users/juspay/ny/nammayatri/Backend/app/provider-platform/dynamic-offer-driver-app/'"
-    "code-other-rider='code /Users/juspay/nammayatri/Backend/app/rider-platform/rider-app/'"
-    "code-rider='code /Users/juspay/nammayatri/Backend/app/rider-platform/rider-app/'"
     "da='direnv allow'"
-    "flushall='sh /Users/juspay/NammaYatriMagic/flushall.sh'"
+    "flushall='sh $magic_root/flushall.sh'"
     "gb='git branch'"
     "gbc='git checkout -b'"
     "gco='git checkout'"
@@ -27,23 +21,22 @@ aliases=(
     "gprom='git pull --rebase origin main'"
     "gsta='git stash --include-untracked'"
     "gstaa='git stash apply'"
-    "kernel='cd /Users/juspay/shared-kernel'"
-    "kill-server='sh /Users/juspay/NammaYatriMagic/portkill.sh'"
-    "magic='cd ~/NammaYatriMagic/'"
-    "ny='cd /Users/juspay/nammayatri/'"
-    "other-backend='cd /Users/juspay/ny/nammayatri/Backend'"
+    "kill-server='sh $magic_root/portkill.sh'"
+    "magic='cd $magic_root'"
+    "ny='cd $project_root'"
+    "backend='cd $project_root/Backend'"
     "pgadmin='nix run .#arion -- up --remove-orphans pg-admin'"
     "prune='git gc --prune=now'"
     "qpgadmin='nix run .#arion -- up --remove-orphans -d pg-admin'"
     "qsvc='nix run .#arion -- up --remove-orphans -d'"
     "repl-driver='cabal repl dynamic-offer-driver-app'"
     "repl-rider='cabal repl rider-app'"
-    "ride-flow='sh ~/NammaYatriMagic/ride-flow.sh'"
-    "rms='sh /Users/juspay/NammaYatriMagic/start.sh'"
-    "run-ride='newman run /Users/juspay/NammaYatriMagic/NammaYatri-RideBooking.json'"
+    "ride-flow='sh $magic_root/ride-flow.sh'"
+    "rms='sh $magic_root/start.sh'"
+    "run-ride='newman run $magic_root/NammaYatri-RideBooking.json'"
     "run-server='nix run .#run-mobility-stack-dev'"
     "sac='nix run .#arion -- down --remove-orphans'"
-    "server-cold-start='sh /Users/juspay/NammaYatriMagic/server-start.sh'"
+    "server-cold-start='sh $magic_root/server-start.sh'"
     "svc=', run-svc'"
 )
 
