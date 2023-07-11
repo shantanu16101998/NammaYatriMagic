8fc5d7ffd304129e59d276cfc2255ec25f5ec823
Shantanu Mukherjee <shantanu.mukherjee@juspay.in>
2023-07-11 18:19:35 +0530

    commit

--- random-driver.sh
+++ random-driver.sh
@@ -4,5 +4,5 @@
 random_number=$(shuf -i 1000000000-9999999999 -n 1)
 
 echo "[+] Driver number is $random_number"
-python3 /Users/juspay/NammaYatriMagic/replace-driver-number.py $random_number
+python3 /Users/juspay/NammaYatriMagic/replace-number.py $random_number
 python3 /Users/juspay/NammaYatriMagic/register-driver.py $random_number
